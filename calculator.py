ca1db4363d36b425c00d83b3d4c058d1e7e6f5a7
Nischay Suthar <138374135+nischaysuthar@users.noreply.github.com>
2023-09-20 21:40:39 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+m=4
+n=5
+
+print("\nthe value of m",m)
+print("\nthe value of n",n)
+
+sum=m+n
+print("\nsum of m and n",sum)
+
+subtraction=m-n
+print("\nsubtraction of m and n",subtraction)
+
+#other method
+print("\nthe value of m",m,"/",15,m/n)
+
+multiplication=m*n
+print("\nmultiplication of m and n",multiplication)
+
+exponential=m**n
+print("\nexponential is",exponential)
+
+floor=m//n
+print("\nfloor division is",floor)
+
+square1=pow(m,2)
+square2=pow(n,2)
+
+print("\n square is",square1)
+print("\n square is",square2)
+
+cube1=pow(m,3)
+cube2=pow(n,3)
+
+print("\n cube is",cube1)
+print("\n cube is",cube2)
